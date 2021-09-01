2d6d92f09b66412a8463493b6fda6de06ad52a49
julioclopes32 <julioclopes32@gmail.com>
2021-09-01 18:33:19 -0300

    Update Learning Exercise - Guido's Gorgeous Lasagna.py

--- python/Learning Exercise - Guido's Gorgeous Lasagna.py
+++ python/Learning Exercise - Guido's Gorgeous Lasagna.py
@@ -4,21 +4,25 @@ Define an EXPECTED_BAKE_TIME constant that returns how many minutes the lasagna 
 
 >> lasagna.EXPECTED_BAKE_TIME
 40
+
 2. Calculate remaining bake time in minutes
 Implement the bake_time_remaining() function that takes the actual minutes the lasagna has been in the oven as an argument and returns how many minutes the lasagna still needs to bake based on the EXPECTED_BAKE_TIME.
 
 >> bake_time_remaining(30)
 10
+
 3. Calculate preparation time in minutes
 Implement the preparation_time_in_minutes() function that takes the number of layers you want to add to the lasagna as an argument and returns how many minutes you would spend making them. Assume each layer takes 2 minutes to prepare.
 
 >> preparation_time_in_minutes(2)
 4
+
 4. Calculate total elapsed cooking time (prep + bake) in minutes
 Implement the elapsed_time_in_minutes() function that has two parameters: number_of_layers (the number of layers added to the lasagna) and elapsed_bake_time (the number of minutes the lasagna has been baking in the oven). This function should return the total number of minutes you've been cooking, or the sum of your preparation time and the time the lasagna has already spent baking in the oven.
 
 >> elapsed_time_in_minutes(3, 20)
 26
+
 5. Update the recipe with notes
 Go back through the recipe, adding notes and documentation.
 
