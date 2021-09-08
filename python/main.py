7c372efb0895c17afae41efa1c96e10021194288
julioclopes32 <julioclopes32@gmail.com>
2021-09-08 01:24:47 -0300

    Learning Exercise - Ghost Gobble Arcade Game

--- python/main.py
+++ python/main.py
@@ -3,14 +3,54 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    @property
+    def likes(self):
+        return self._likes
 
+    def dar_likes(self):
+        self._likes += 1
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    @property
+    def nome(self):
+        return self._nome
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        self.__duracao = duracao
+
+    @property
+    def duracao(self):
+        return self.__duracao
+
+    @duracao.setter
+    def duracao(self, duracao):
+        self.__duracao = duracao
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        self.__temporadas = temporadas
+
+    @property
+    def temporadas(self):
+        return self.temporadas
+
+    @temporadas.setter
+    def temporadas(self, temporadas):
+        self.temporadas = temporadas
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+atlanta = Serie('atlanta', 2018, 2)
+
+print(vingadores.nome)
+print(vingadores.duracao)
+
